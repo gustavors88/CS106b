2679b7bb66dc3021149bdc031dbf6a52c5f47344
jeremymwat <Jeremymwatson@gmail.com>
2013-08-06 22:04:41 -0400

    add more c++

---
+++ lectures/lectures/lecture02a.cpp
@@ -0,0 +1,21 @@
+/* Created by Jeremy
+*
+*
+*
+*/
+
+
+#include <iostream>
+#include "simpio.h"
+using namespace std;
+
+int getInteger();
+
+int main()
+{
+	cout << "How much do you love 106B ";
+	int howAwesome = getInteger();
+	for (int i = 0; i = howAwesome; i++)
+		cout << "some" << endl;
+	return 0;
+}
