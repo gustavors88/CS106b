4e7cc044d8552938a6eb40c4af8d19c8c7f3fe5c
Jeremy W <jeremymwatson@gmail.com>
2013-08-07 16:53:48 -0400

    work on chapter 1 review

---
+++ sandbox/lectures/convertMetersToImp.cpp
@@ -0,0 +1,18 @@
+/* Created by Jeremy
+*
+*
+*
+*/
+
+
+#include <iostream>
+#include "simpio.h"
+#include <string>
+using namespace std;
+
+void convertMetersToImp()
+{
+	cout << "Enter meters to be converted: ";
+	double rawInp = getReal();
+	
+}
